5dc105b73b24694e471a7fa038666ab0490f1392
niallodev <nloorbazurto@gmail.com>
2024-12-12 16:11:38 -0500

    Ejecutando el comando para crear el build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portafolio_Frontend/', // Reemplaza con el nombre de tu repositorio
   plugins: [react()],
+  build: {
+    outDir: 'build', // Cambia el nombre de la carpeta generada
+  },
 })
