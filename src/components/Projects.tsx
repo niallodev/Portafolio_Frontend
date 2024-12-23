e2a8acb8a80b3872545a473a442362d13f726234
niallodev <nloorbazurto@gmail.com>
2024-12-23 18:08:38 -0500

    Agregado el primer proyecto el Commic ademas de unos cambios en la sección Proyectos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import { FaGithub, FaEye } from 'react-icons/fa'; // Para agregar iconos de GitHub y vista previa
-import { FaReact, FaNodeJs, FaDatabase, FaCss3, FaHtml5, FaBrain } from 'react-icons/fa'; // Ejemplo de íconos
+import { FaReact, FaNodeJs, FaDatabase, FaCss3, FaHtml5, FaBrain, FaRobot } from 'react-icons/fa'; // Ejemplo de íconos
 import Slider from 'react-slick'; // Carrusel
 import "slick-carousel/slick/slick.css"; // Importa estilos básicos
 import "slick-carousel/slick/slick-theme.css"; // Importa estilos del tema
@@ -8,29 +8,15 @@ import "slick-carousel/slick/slick-theme.css"; // Importa estilos del tema
 function Projects() {
   const projects = [
     {
-      title: 'Proyecto 1',
+      title: 'Commic - El Pacto de los Espectros',
       // description: 'Descripción breve del proyecto 1. Aquí puedes hablar de las tecnologías usadas y la solución que ofreciste.',
-      description: 'El proyecto "El Pacto de los Espectros" es un cómic interactivo desarrollado para mostrar habilidades técnicas y como regalo personal. Utiliza tecnologías web como HTML, CSS y React para crear una experiencia visual dinámica. Las ilustraciones fueron generadas con la ayuda de Leonardo.ai, incorporando un estilo único y detallado. Además, se implementaron herramientas de colaboración como Git y GitHub para el control de versiones y el trabajo en equipo. Este proyecto tiene como objetivo combinar narrativa visual con tecnologías modernas.',
+      description: 'Es un cómic interactivo desarrollado para mostrar habilidades técnicas y como regalo personal. Utiliza tecnologías web como HTML, CSS y React para crear una experiencia visual dinámica. Las ilustraciones fueron generadas con la ayuda de Leonardo.ai, incorporando un estilo único y detallado. Además, se implementaron herramientas de colaboración como Git y GitHub para el control de versiones y el trabajo en equipo. Este proyecto tiene como objetivo combinar narrativa visual con tecnologías modernas.',
       demoLink: 'https://niallodev.github.io/Commic-El-Pacto-de-los-Espectros/',  // Enlace de la demo
       githubLink: 'https://github.com/niallodev/Commic-El-Pacto-de-los-Espectros', // Enlace de GitHub
-      technologies: [<FaReact />, <FaCss3 />, <FaHtml5 />,  <FaGithub />, <FaBrain /> ], // Íconos de tecnologías
+      technologies: [<FaReact />, <FaCss3 />, <FaHtml5 />,  <FaGithub />, <FaRobot />, <FaBrain /> ], // Íconos de tecnologías,
+      image: 'https://niallodev.github.io/Commic-El-Pacto-de-los-Espectros/img/pagina_1_cuadro_1.jpg'
     },
-    {
-      title: 'Proyecto 2',
-      description: 'Descripción breve del proyecto 2. Explica un desafío que resolviste o una característica destacada.',
-      demoLink: '#',
-      githubLink: '#',
-      technologies: [<FaReact />, <FaDatabase />],
-      // technologies: [<FaReact />],
-    },
-    {
-      title: 'Proyecto 3',
-      description: 'Descripción breve del proyecto 3. Incluye un logro o el impacto que tuvo este proyecto.',
-      demoLink: '#',
-      githubLink: '#',
-      technologies: [<FaNodeJs />, <FaReact />],
-      // technologies: [<FaNodeJs />],
-    },
+   
   ];
 
   const sliderSettings = {
@@ -45,7 +31,7 @@ function Projects() {
     pauseOnHover: true, // Pausar el autoplay cuando el usuario pasa el cursor sobre el carrusel
     centerMode: true, // Centrar los elementos visibles
     centerPadding: '0px', // Espaciado entre elementos
-    arrows: true, // Habilitar flechas
+    arrows: false, // Habilitar flechas
     fade: false,  // Efecto de desvanecimiento en vez de desplazamiento
     swipeToSlide: true, // Permite deslizar para cambiar
     touchThreshold: 15, // Sensibilidad al toque
@@ -70,50 +56,65 @@ function Projects() {
       },
     ],
   };
+
+
   
   
   return (
     <section id="projects" className="py-20 px-5 bg-gray-100 text-center">
       <h2 className="text-4xl font-extrabold mb-12 text-teal-600">Mis Proyectos</h2>
       <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-5xl mx-auto">
-        {projects.map((project, index) => (
-          <div
-            key={index}
-            className="transform transition-all duration-300 ease-in-out bg-white rounded-lg shadow-lg p-4 hover:scale-105 hover:shadow-2xl hover:translate-y-2"
-          >
-            <h3 className="text-2xl font-semibold mb-4">{project.title}</h3>
-            <p className="text-gray-600 mb-4">{project.description}</p>
-            <div className="px-4 items-center">
-              <Slider {...sliderSettings}>
-                {project.technologies.map((tech, i) => (
-                  <div key={i} className="text-4xl text-teal-600 flex justify-center items-center text-center">
-                
-                    <tech.type {...tech.props} className="m-auto" />
-                    
-                  </div>
-                ))}
-              </Slider>
-            </div>
-            <div className="flex justify-between">
-              <a
-                href={project.demoLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-teal-600 hover:text-teal-800 flex items-center"
-              >
-                <FaEye className="mr-2" /> Ver Demo
-              </a>
-              <a
-                href={project.githubLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-teal-600 hover:text-teal-800 flex items-center"
-              >
-                <FaGithub className="mr-2" /> GitHub
-              </a>
-            </div>
+      {projects.map((project, index) => (
+        <div key={index} className="flip-card">
+        <div className="flip-card-inner">
+          {/* <!-- Parte frontal --> */}
+          <div className="flip-card-front">
+            <h3 className="text-2xl font-semibold absolute top-3 text-white">{project.title}</h3>
+            <img src={project.image} alt="Imagen Frontal"/>
           </div>
-        ))}
+          {/* <!-- Parte trasera --> */}
+          <div className="flip-card-back  text-teal-600 block justify-center items-center text-center">
+          
+          <h3 className="text-2xl font-semibold mb-4">{project.title}</h3>
+              <p className="text-gray-600 mb-4">{project.description}</p>
+              <div className="px-4 items-center">
+                <Slider {...sliderSettings}>
+                  {project.technologies.map((tech, i) => (
+                    <div key={i} className="text-4xl text-teal-600 flex justify-center items-center text-center">
+                  
+                      <tech.type {...tech.props} className="m-auto" />
+                      
+                    </div>
+                  ))}
+                </Slider>
+              </div>
+              <br />
+              <div className="flex justify-between">
+                <a
+                  href={project.demoLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-teal-600 hover:text-teal-800 flex items-center z-10"
+                >
+                  <FaEye className="mr-2" /> Ver Demo
+                </a>
+                <a
+                  href={project.githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-teal-600 hover:text-teal-800 flex items-center z-10"
+                >
+                  <FaGithub className="mr-2" /> GitHub
+                </a>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      ))}
+      
+       
+        
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,17 +11,17 @@ export default {
         serif: ['Times New Roman', 'serif'], // Configuración para Times
       },
       fontSize: {
-        'xs': ['0.625rem', { lineHeight: '1rem' }], // Más pequeño
-        'sm': ['0.75rem', { lineHeight: '1.25rem' }], // 640 px	Pantallas pequeñas (teléfonos)
-        'base': ['0.875rem', { lineHeight: '1.5rem' }],  // 	768 px	Pantallas medianas (tabletas)
-        'lg': ['1rem', { lineHeight: '1.5rem' }], // 1024 px	Pantallas grandes (laptops pequeñas)
-        'xl': ['1.125rem', { lineHeight: '1.75rem' }], // 1280 px	Pantallas muy grandes (laptops grandes)
-        '2xl': ['1.375rem', { lineHeight: '1.75rem' }], // 1536 px	Pantallas extra grandes (monitores)
-        '3xl': ['1.5rem', { lineHeight: '2rem' }],
+        'sm': ['0.625rem', { lineHeight: '1rem' }], // Más pequeño
+        'base': ['0.75rem', { lineHeight: '1.25rem' }], // 640 px	Pantallas pequeñas (teléfonos)
+        'lg': ['0.875rem', { lineHeight: '1.5rem' }],  // 	768 px	Pantallas medianas (tabletas)
+        'xl': ['1rem', { lineHeight: '1.5rem' }], // 1024 px	Pantallas grandes (laptops pequeñas)
+        '2xl': ['1.125rem', { lineHeight: '1.75rem' }], // 1280 px	Pantallas muy grandes (laptops grandes)
+        '3xl': ['1.375rem', { lineHeight: '1.75rem' }], // 1536 px	Pantallas extra grandes (monitores)
+        '4xl': ['1.5rem', { lineHeight: '2rem' }],
         '4xl': ['1.75rem', { lineHeight: '2rem' }],
         '5xl': ['2rem', { lineHeight: '2.25rem' }],
         '6xl': ['2.25rem', { lineHeight: '2.5rem' }],
-        
+        // @apply text-base sm:text-lg md:text-xl lg:text-2xl;
       },
     },
   },
